d3b0ae8019256084c2fc768d01fefdcd2f2d360d
Ashley Schroder <ashley.schroder@gmail.com>
2012-07-03 22:43:51 +1200

    fix for false negative on self test when user has a custom contact form template

--- Model/Observer.php
+++ Model/Observer.php
@@ -23,8 +23,7 @@ class Aschroder_SMTPPro_Model_Observer {
 		}
 		
 		// For the self test, if we're sending the contact form notify the self test class
-		Mage::log("template=" . $event->getTemplate());
-		if($event->getTemplate() == "contacts_email_email_template") {
+		if($event->getTemplate() == Mage::getStoreConfig("contacts/email/email_template")){
 			include_once Mage::getBaseDir() . "/app/code/community/Aschroder/SMTPPro/controllers/IndexController.php";
 			Aschroder_SMTPPro_IndexController::$CONTACTFORM_SENT = true;
 		}
