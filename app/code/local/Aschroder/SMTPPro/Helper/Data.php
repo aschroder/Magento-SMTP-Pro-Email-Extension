31cf0beb71e181da6e1775ecaf009f894edd905d
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-01-27 08:31:46 +0000

    Make AWS region configurable

--- app/code/local/Aschroder/SMTPPro/Helper/Data.php
+++ app/code/local/Aschroder/SMTPPro/Helper/Data.php
@@ -101,6 +101,11 @@ class Aschroder_SMTPPro_Helper_Data extends Mage_Core_Helper_Abstract
         return Mage::getStoreConfig('smtppro/general/ses_private_key', $storeId);
     }
 
+    public function getAmazonSESRegion($storeId = null)
+    {
+        return Mage::getStoreConfig('smtppro/general/ses_region', $storeId);
+    }
+
     public function getGoogleAppsEmail($storeId = null)
     {
         return Mage::getStoreConfig('smtppro/general/googleapps_email', $storeId);
