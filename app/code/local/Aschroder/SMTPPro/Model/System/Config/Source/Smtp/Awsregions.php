655c76049a4e83d72be7b11ee0e59227e887c027
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-01-27 20:59:10 +0000

    Removed regions where SES sending is not available

--- app/code/local/Aschroder/SMTPPro/Model/System/Config/Source/Smtp/Awsregions.php
+++ app/code/local/Aschroder/SMTPPro/Model/System/Config/Source/Smtp/Awsregions.php
@@ -10,14 +10,7 @@ class Aschroder_SMTPPro_Model_System_Config_Source_Smtp_Awsregions
         return array(
             'us-east-1' => Mage::helper('adminhtml')->__('US East (N. Virginia)'),
             'us-west-2' => Mage::helper('adminhtml')->__('US West (Oregon)'),
-            'us-west-1' => Mage::helper('adminhtml')->__('US West (N. California)'),
-            'eu-west-1' => Mage::helper('adminhtml')->__('EU (Ireland)'),
-            'eu-central-1' => Mage::helper('adminhtml')->__('EU (Frankfurt)'),
-            'ap-southeast-1' => Mage::helper('adminhtml')->__('Asia Pacific (Singapore)'),
-            'ap-northeast-1' => Mage::helper('adminhtml')->__('Asia Pacific (Tokyo)'),
-            'ap-southeast-2' => Mage::helper('adminhtml')->__('Asia Pacific (Sydney)'),
-            'ap-northeast-2' => Mage::helper('adminhtml')->__('Asia Pacific (Seoul)'),
-            'sa-east-1' => Mage::helper('adminhtml')->__('South America (Sao Paulo)')
+            'eu-west-1' => Mage::helper('adminhtml')->__('EU (Ireland)')
         );
     }
 }
