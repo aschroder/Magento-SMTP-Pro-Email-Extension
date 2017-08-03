f4c79e9c73ad07e6cabd149a55a587b8ffa6df5e
Pascal Brouwers <pascal@h-o.nl>
2017-08-03 14:23:18 +0200

    [FIN-631] Log failed emails through observer and then remove them from the queue

--- app/code/local/Aschroder/SMTPPro/Model/Email/Queue.php
+++ app/code/local/Aschroder/SMTPPro/Model/Email/Queue.php
@@ -116,8 +116,6 @@ class Aschroder_SMTPPro_Model_Email_Queue extends Mage_Core_Model_Email_Queue {
                             'html' => !$parameters->getIsPlain(),
                             'email_body' => $message->getMessageBody()));
                     }
-
-
                 }
                 catch (Exception $e) {
                     unset($mailer);
@@ -125,6 +123,24 @@ class Aschroder_SMTPPro_Model_Email_Queue extends Mage_Core_Model_Email_Queue {
                     Mage::setIsDeveloperMode(true);
                     Mage::logException($e);
                     Mage::setIsDeveloperMode($oldDevMode);
+
+                    // Was the that the email was invalid? then log it into the grid.
+                    if (strpos($e->getMessage(), 'Recipient syntax error') !== false) {
+                        // loop each email to fire a failed send event
+                        foreach ($message->getRecipients() as $recipient) {
+                            list($email, $name, $type) = $recipient;
+                            Mage::dispatchEvent('aschroder_smtppro_failed_send', array(
+                                'to' => $email,
+                                'template' => "queued email",
+                                // TODO: should we preserve the template id in the queue object, in order to include it here?
+                                'subject' => $parameters->getSubject(),
+                                'html' => !$parameters->getIsPlain(),
+                                'email_body' => $message->getMessageBody()));
+                        }
+
+                        // remove the message from the queue
+                        $message->delete();
+                    }
 
                     continue;
                 }
