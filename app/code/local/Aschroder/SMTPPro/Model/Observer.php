3d6efe77901a94f536960ca65cc7cdf70028a106
Raul Sanchez <raul.sanchez@viansolutions.com>
2019-01-21 13:53:34 +0100

    Update Observer.php

--- app/code/local/Aschroder/SMTPPro/Model/Observer.php
+++ app/code/local/Aschroder/SMTPPro/Model/Observer.php
@@ -63,7 +63,7 @@ class Aschroder_SMTPPro_Model_Observer extends Varien_Object {
 
     public function beforeSendQueue($observer) {
         Mage::helper('smtppro')->log($observer->getEvent()->getMail());
-        $observer->getEvent()->getTransport()->setTransport(Mage::helper('smtppro')->getTransport());
+        $observer->getEvent()->getTransport()->setTransport(Mage::helper('smtppro')->getTransport($observer->getEvent()->getStoreId()));
     }
 	
 }
